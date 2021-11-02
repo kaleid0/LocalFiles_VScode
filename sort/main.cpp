13fdb3ad52e18c10562d2377a3f69af2170df455
kaleid0 <nibiewgnahz@qq.com>
2021-11-02 21:27:14 +0800

    添加归并排序

--- sort/main.cpp
+++ sort/main.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 #include<time.h>
 #include"sort.h"
-
+#include"sort.cpp"
+#include<iomanip>
 using namespace std;
 
 
-const int num = 50000;
+//const int num = 50000;
 
 
 int main(){
@@ -18,35 +19,39 @@ int main(){
     fun[5] = quicksort;
     fun[6] = selectsort;
     fun[7] = heapsort;
+    fun[8] = mergesort;
 
-    // int begin = clock();
-    // bubblesort2(a, num);
-    // int end = clock();
-    // show(a, 20);
-    // cout << double(end - begin) / CLOCKS_PER_SEC;
+    // int A[12] = {7, 5, 12, 3, 2, 1, 6, 11, 10, 4, 8, 9};
 
-    double time[30][8];
-    for (int i = 0; i < 1; i++){
+    // mergesort(A, 12);
+
+    // for (int i = 0; i < 12; i++)
+    //     cout << A[i] << ',';
+
+    double time[30][9];
+    for (int i = 0; i < 10; i++){
         int *a = rondam(num);
         int B[num];
         copy(a, a + num-1000, B);
         cout << i << endl;
-        for (int j = 0; j < 8;j++){
+        for (int j = 0; j < 9;j++){
             time[i][j] = sort(fun[j], a, num-100);
         }
         delete[] a;
     }
 
-    double avgtime[8];
-    for (int i = 0; i < 8; i++){
+    double avgtime[9];
+    for (int i = 0; i < 9; i++){
         avgtime[i] = 0;
-        for (int j = 0; j < 30; j++){
+        for (int j = 0; j < 10; j++){
             avgtime[i] += time[j][i];
         }
-        avgtime[i] /= 30;
-        cout << avgtime[i] << endl;
+        avgtime[i] /= 10;
+        cout << fixed;
+        cout << setprecision(5) << avgtime[i] << endl;
     }
 
-    
+    system("pause");
+
     return 0;
 }
